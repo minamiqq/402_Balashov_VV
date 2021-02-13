54b6f9bb12d2a31f155a407ec0873b344ba47dd8
Vlad <balashovv.vlad@gmail.com>
2021-02-13 11:50:43 +0300

    Task02

---
+++ Task02_2/src/StudentsList.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use App\Student;
+use Exeption as Exeption;
+
+class StudentsList
+{
+    private $students = array();
+
+    public function add(Student $student)
+    {
+        array_push($this -> students, $student);
+    }
+
+    public function count(): int
+    {
+        return count($this -> students);
+    }
+
+    public function get(int $n): Student
+    {
+        if ($n <= 0 || $n > count($this -> students)) {
+            return "Недопоустимое значение";
+        }
+        return $this -> students[$n - 1];
+    }
+
+    public function store(string $fileName)
+    {
+        file_put_contents($fileName, serialize($this -> students));
+    }
+
+    public function load(string $fileName)
+    {
+        if (!file_exists($fileName)) {
+            return "Файл " . $fileName . " не существует!";
+        }
+
+        $this -> students = unserialize(file_get_contents($fileName));
+    }
+}

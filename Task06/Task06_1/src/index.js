7953394afc7a0107beaf44ffe11dee831e6e10e7
Vlad <balashovv.vlad@gmail.com>
2021-04-08 14:12:44 +0300

    Task06_1

--- Task06/Task06_1/src/index.js
+++ Task06/Task06_1/src/index.js
@@ -1,43 +1,43 @@
 function createVector(x, y, z) {
-	return {
-		x,
-		y,
-		z,
+  return {
+    x,
+    y,
+    z,
 
-		getLength() {
-			return Math.sqrt(x ** 2 + y ** 2 + z ** 2)
-		},
-		add(vector) {
-			return createVector(
-				this.x + vector.x,
-				this.y + vector.y,
-				this.z + vector.z
-			)
-		},
-		sub(vector) {
-			return createVector(
-				this.x - vector.x,
-				this.y - vector.y,
-				this.z - vector.z
-			)
-		},
-		product(number) {
-			return createVector(this.x * number, this.y * number, this.z * number)
-		},
-		scalarProduct(vector) {
-			return this.x * vector.x + this.y * vector.y + this.z * vector.z
-		},
-		vectorProduct(vector) {
-			return createVector(
-				this.y * vector.z - this.z * vector.y,
-				-(this.x * vector.z - this.z * vector.x),
-				this.x * vector.y - this.y * vector.x
-			)
-		},
-		toString() {
-			return `(${this.x};${this.y};${this.z})`
-		},
-	}
+    getLength() {
+      return Math.sqrt(x ** 2 + y ** 2 + z ** 2)
+    },
+    add(vector) {
+      return createVector(
+        this.x + vector.x,
+        this.y + vector.y,
+        this.z + vector.z
+      )
+    },
+    sub(vector) {
+      return createVector(
+        this.x - vector.x,
+        this.y - vector.y,
+        this.z - vector.z
+      )
+    },
+    product(number) {
+      return createVector(this.x * number, this.y * number, this.z * number)
+    },
+    scalarProduct(vector) {
+      return this.x * vector.x + this.y * vector.y + this.z * vector.z
+    },
+    vectorProduct(vector) {
+      return createVector(
+        this.y * vector.z - this.z * vector.y,
+        -(this.x * vector.z - this.z * vector.x),
+        this.x * vector.y - this.y * vector.x
+      )
+    },
+    toString() {
+      return `(${this.x};${this.y};${this.z})`
+    },
+  }
 }
 
 module.exports = createVector
